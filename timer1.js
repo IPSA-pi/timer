bcdacb48cd6b6133b2557994adef4d7a9c8a02c7
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-10 13:28:36 -0700

    implemented timmer function with edge cases

--- timer1.js
+++ timer1.js
@@ -1,16 +1,17 @@
 /* eslint-disable max-len */
 const args = process.argv.slice(2).map((a) => Number(a));
-
+if (args.length === 0) {
+  return;
+}
 const filtered = args.filter((arg) => Number.isNaN(arg) !== true && arg > 0 );
 
 filtered.map((a) => {
   setTimeout(() => {
-    if (args.length === 0) {
-      return;
-    }
+    
     process.stdout.write('\x07');
   }, a * 1000);
 });
 
+// eslint Parsing error: 'return' outside of functioneslint line 4 ???
 // the closest interval I can use to output system sounds on my system is 1100 ms
 // process.stdout.write('\x07');
