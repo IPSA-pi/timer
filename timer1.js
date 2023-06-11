9ac2a564a2325f1ea1ae9c50536b3c45a3063b23
Ian Sebleius <sebeliusancira@gmail.com>
2023-06-11 11:44:25 -0700

    made changes based on code review by Omobowale Otuyiga

--- timer1.js
+++ timer1.js
@@ -1,13 +1,15 @@
 /* eslint-disable max-len */
+
+// get command line args passed by the user and convert them to number
 const args = process.argv.slice(2).map((a) => Number(a));
-if (args.length === 0) {
-  return;
-}
+
+// filter the arguments array and only keep values that aren't NaN and are greater than 0
 const filtered = args.filter((arg) => Number.isNaN(arg) !== true && arg > 0 );
 
+// map over each filtered argument, and for each one we'll create a setTimeout that outputs sound
+// after the defined time by the user in secs (we multiply it by a 1000 since setTimeout's 2nd arg should be in ms.).
 filtered.map((a) => {
   setTimeout(() => {
-    
     process.stdout.write('\x07');
   }, a * 1000);
 });
